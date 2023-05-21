e58bd81cad93f284a6d7d60e66f9f5716153daf6
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 12:20:03 +0530

    Implement a method add that takes a string argument and returns an integer

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,12 +1,9 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+
 public class StringCalculator {
-    @Test
-    void testAddEmptyString() {
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.add("");
-        Assertions.assertEquals(0, result);
+    public int add(String numbers) {
+        return 0;
     }
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,11 @@
+import org.example.StringCalculator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 public class StringCalculatorTest {
+    @Test
+    void testAddEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("");
+        Assertions.assertEquals(0, result);
+    }
 }
