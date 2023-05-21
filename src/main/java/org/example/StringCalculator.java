cf36a7dafa69e4c4cd6cdb0b5daaa3578c0720a0
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 12:34:48 +0530

    Modify the add method to handle multiple numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -6,7 +6,12 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         } else {
-            return Integer.parseInt(numbers);
+            String[] numberArray = numbers.split(",");
+            int sum = 0;
+            for (String number : numberArray) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
         }
     }
 
